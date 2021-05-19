b1024251e9c641a41b7726f966eebe24e70d7786
crogar <58400421+crogar@users.noreply.github.com>
2021-05-19 18:43:07 -0400

    Update app.py
    Added basic flask App

--- app.py
+++ app.py
@@ -10,3 +10,25 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func, inspect
 
 from flask import Flask, jsonify
+
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+app.config["JSON_SORT_KEYS"] = False # Preventing Jsonify to reorder the dictionario elements by the keys
+#################################################
+# Flask Routes
+#################################################
+
+
+@app.route("/")
+def index():
+    """Index - Landing Page"""
+    return (
+        f"<h1>Index Page</h1>"
+    )
+
+
+if __name__ == "__main__":
+    app.run(debug=True  )
