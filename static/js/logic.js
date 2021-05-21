089b0411a2f4fde04bea492a64ceb5804a2506af
crogar <58400421+crogar@users.noreply.github.com>
2021-05-20 22:56:51 -0400

    reading geoJson data

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,4 +1,5 @@
 geojson_url = "https://opendata.arcgis.com/datasets/0573e90adab5434f97b082590c503bc1_0.geojson"
+
 d3.json(geojson_url).then(data => {
     console.log(data);
 }); // End reading geoJson
