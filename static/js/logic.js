f9ac1d7156db6a3364e6550232696fc78ea2b510
crogar <58400421+crogar@users.noreply.github.com>
2021-05-23 14:17:57 -0400

    adding maps

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,5 +1,25 @@
-geojson_url = "https://opendata.arcgis.com/datasets/0573e90adab5434f97b082590c503bc1_0.geojson"
-
-d3.json(geojson_url).then(data => {
-    console.log(data);
-}); // End reading geoJson
+// Creating map object
+var myMap = L.map("map", {
+    center: [38.9072, -77.0369],
+    zoom: 7
+  });
+  
+  // Adding tile layer
+  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+  }).addTo(myMap);
+  
+  // Use this link to get the geojson data.
+  var link = "https://opendata.arcgis.com/datasets/4c172f80b626490ea2cff7b699febedb_1.geojson";
+  
+  // Grabbing our GeoJSON data..
+  d3.json(link).then(function(data) {
+    console.log(data)
+    // Creating a GeoJSON layer with the retrieved data
+    L.geoJson(data).addTo(myMap);
+  });
