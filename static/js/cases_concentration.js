c261c3fabf6a23598f0ad0f51bfc0e933e6f7232
crogar <58400421+crogar@users.noreply.github.com>
2021-05-26 15:17:01 -0400

    Choropleth visualization generated

--- static/js/cases_concentration.js
+++ static/js/cases_concentration.js
@@ -7,7 +7,7 @@ var myMap = L.map("map", {
   
   // Adding tile layer
   L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    // attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
     maxZoom: 18,
     zoomOffset: -1,
@@ -15,69 +15,71 @@ var myMap = L.map("map", {
     accessToken: API_KEY
   }).addTo(myMap);
   
-// Use this link to get the geojson data.
-var link = "../static/js/maryland_geojson.geojson";
 
-// Function that will determine the color of a neighborhood based on the borough it belongs to
-function chooseColor(borough) {
-  switch (borough) {
-  case "Brooklyn":
-    return "yellow";
-  case "Bronx":
-    return "red";
-  case "Manhattan":
-    return "orange";
-  case "Queens":
-    return "green";
-  case "Staten Island":
-    return "purple";
-  default:
-    return "black";
-  }
-}
+// Load in geojson data
+var geoData = "../static/js/maryland_geojson.geojson";
 
-// Grabbing our GeoJSON data..
-d3.json(link).then(function(data) {
+var geojson;
+
+// Grab data with d3
+d3.json(geoData).then(function(data) {
   console.log(data)
-  // Creating a geoJSON layer with the retrieved data
-  L.geoJson(data, {
-    // Style each feature (in this case a neighborhood)
-    style: function(feature) {
-      console.log(feature.properties.confirmed_cases)
-      return {
-        color: "white",
-        // Call the chooseColor function to decide which color to color our neighborhood (color based on borough)
-        fillColor: chooseColor(feature.properties.borough),
-        fillOpacity: 0.5,
-        weight: 1.5
-      };
-    },
-    // Called on each feature
-    onEachFeature: function(feature, layer) {
-      // Set mouse events to change map styling
-      layer.on({
-        // When a user's mouse touches a map feature, the mouseover event calls this function, that feature's opacity changes to 90% so that it stands out
-        mouseover: function(event) {
-          layer = event.target;
-          layer.setStyle({
-            fillOpacity: 0.9
-          });
-        },
-        // When the cursor no longer hovers over a map feature - when the mouseout event occurs - the feature's opacity reverts back to 50%
-        mouseout: function(event) {
-          layer = event.target;
-          layer.setStyle({
-            fillOpacity: 0.5
-          });
-        },
-        // When a feature (neighborhood) is clicked, it is enlarged to fit the screen
-        click: function(event) {
-          myMap.fitBounds(event.target.getBounds());
-        }
-      });
-      // Giving each feature a pop-up with information pertinent to it
-      layer.bindPopup("<h1>" + feature.properties.neighborhood + "</h1> <hr> <h2>" + feature.properties.borough + "</h2>");
+  // Create a new choropleth layer
+  geojson = L.choropleth(data, {
 
+    // Define what  property in the features to use
+    valueProperty: "confirmed_cases",
+
+    // Set color scale
+    scale: ["#ffffb2", "#b10026"],
+
+    // Number of breaks in step range
+    steps: 10,
+
+    // q for quartile, e for equidistant, k for k-means
+    mode: "q",
+    style: {
+      // Border color
+      color: "#fff",
+      weight: 1,
+      fillOpacity: 0.8
+    },
+
+    // Binding a pop-up to each layer
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup("County: " + feature.properties.county + "<br>Number of Confirmed Cases:<br>" +
+        feature.properties.confirmed_cases);
     }
   }).addTo(myMap);
+
+  // Set up the legend
+  // var legend = L.control({ position: "bottomright" });
+  // legend.onAdd = function() {
+  //   var div = L.DomUtil.create("div", "info legend");
+  //   var limits = geojson.options.limits;
+  //   var colors = geojson.options.colors;
+  //   var labels = [];
+
+  //   // Add min & max
+  //   var legendInfo = "<h1>Median Income</h1>" +
+  //     "<div class=\"labels\">" +
+  //       "<div class=\"min\">" + limits[0] + "</div>" +
+  //       "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
+  //     "</div>";
+
+  //   div.innerHTML = legendInfo;
+
+  //   limits.forEach(function(limit, index) {
+  //     labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
+  //   });
+
+  //   div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+  //   return div;
+  // };
+
+  // Adding legend to the map
+  // legend.addTo(myMap);
+
 });
+
+
