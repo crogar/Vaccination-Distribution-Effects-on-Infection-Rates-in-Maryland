df04297763c1deedfd1677a51abe7b46e5816ab7
ccgrace <77289566+ccgrace@users.noreply.github.com>
2021-05-26 20:48:23 -0400

    added county population data

--- static/js/gauge.js
+++ static/js/gauge.js
@@ -1,5 +1,31 @@
 d3.json("./data/samples.json").then((data) => {
+let county_pop = [
+    {County: "Allegany", population: 75300},
+    {County: "Anne Arundel", population: 556100},
+    {County: "Baltimore", population: 842600},
+    {County: "Baltimore City", population: 620961},
+    {County: "Calvert", population: 100450},
+    {County: "Caroline", population: 40300},
+    {County: "Carroll", population: 197400},
+    {County: "Cecil", population: 130350},
+    {County: "Charles", population: 177200},
+    {County: "Dorchester", population: 36300},
+    {County: "Frederick", population: 287900},
+    {County: "Garrett", population: 31600},
+    {County: "Harford", population: 276500},
+    {County: "Howard", population: 312900},
+    {County: "Kent", population: 22200},
+    {County: "Montgomery", population: 1075000},
+    {County: "Prince George's ", population: 921900},
+    {County: "Queen Anne's", population: 55650},
+    {County: "St. Mary's", population: 130100},
+    {County: "Somerset", population: 28300},
+    {County: "Talbot", population: 40050},
+    {County: "Washington", population: 170950},
+    {County: "Wicomico", population: 107450},
+    {County: "Worcester", population: 56250},
 
+]
 
 // Populates the dropdown with subject ID
 function dropDown(County) {
