1cbd4c9adaf7aa56767287d33d15e307f96b2f0e
crogar <58400421+crogar@users.noreply.github.com>
2021-06-01 18:02:13 -0400

    Tables populated on-change event

--- static/js/load_tables.js
+++ static/js/load_tables.js
@@ -1,8 +1,10 @@
-console.log($("#select_dataset :selected").text());
+
 
 function addCode() {
+    var fileName = $("#select_dataset :selected").text() 
+    fileName = fileName.replace('Resources\\', "");
+    document.getElementById("table_name").innerHTML = "<h4>" +  fileName + "</h4>";
     $.getJSON('http://127.0.0.1:5000/gen_table/maryland_covid19-cases.csv', function(data) { // Populating tables
-        console.log(data)
         document.getElementById("table-container").innerHTML += data;
         $(document).ready( function () {
             $('#table_id').DataTable();
@@ -11,3 +13,11 @@ function addCode() {
 }
 
 addCode()
+
+// Event Handler to check when a new date is selected
+$(document).ready(function() {  
+    $('#select_dataset').change(function(){
+        document.getElementById("table-container").innerHTML= "";
+        addCode();
+    });
+  });
