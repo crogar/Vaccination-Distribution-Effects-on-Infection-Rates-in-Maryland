f1ac5abc58bb18b693b1ce082dfe4fcaec5b5866
crogar <58400421+crogar@users.noreply.github.com>
2021-05-31 21:29:42 -0400

    geo_json Vaccination

--- gen_json.py
+++ gen_json.py
@@ -15,23 +15,55 @@ counties = [{'county':"Allegany","coordinates":[39.6255251,-78.6114999]},{'count
 def get_cases(date):
     # date = '2021-05-22'
     # reading SQL 
+    counties_ = counties
     df =pd.read_sql_query('select * from cases', con=engine)
     # Converting Date column from String to Dates
     df['DATE'] = df['DATE'].astype(str)
     last_date = df[df['DATE'] == date]
 
     # Populating Counties with number of cases for that specific date
-    for index,county in enumerate(counties):
+    for index,county in enumerate(counties_):
         dates = date
         cases = last_date[last_date['County'] == county['county']]['Confirmed_cases'].item()
-        counties[index]['date'] = dates
-        counties[index]['Confirmed_cases'] = cases
+        counties_[index]['date'] = dates
+        counties_[index]['Confirmed_cases'] = cases
     # Appending cases and coordinates to Geojson data for Maryland Counties
 
     # Build the endpoint URL
     target_url = 'https://opendata.arcgis.com/datasets/4c172f80b626490ea2cff7b699febedb_1.geojson'
     path_to_file = "./Resources/Maryland_Physical_Boundaries_-_County_Boundaries_(Generalized).geojson"
-    # generating request and converting to json
+    # generating request and co nverting to json
+    geo_data = None
+    with open(path_to_file) as f:
+        geo_data = json.load(f)
+        # Appending Coordinates, date and confirmed cases to our GeoJson Data
+        for county in range(0,len(geo_data['features'])):
+            # Setting the coordinates
+            geo_data['features'][county]['properties']['coordinates'] = counties_[county]['coordinates']
+            # setting confirmed cases and dates
+            geo_data['features'][county]['properties']['date'] = counties_[county]['date']
+            geo_data['features'][county]['properties']['confirmed_cases'] = counties_[county]['Confirmed_cases']
+    return json.dumps(geo_data)
+
+def get_vaccinations(date):
+    # reading SQL 
+    counties = [{'county':"Allegany","coordinates":[39.6255251,-78.6114999]},{'county':"Anne Arundel","coordinates":[38.9530109,-76.5488232]},{'county':"Baltimore","coordinates":[39.4647665,-76.7336521]},{'county':"Baltimore City","coordinates":[39.2903848,-76.6121893]},{'county':"Calvert","coordinates":[38.49495030000001,-76.5025742]},{'county':"Caroline","coordinates":[38.9105018,-75.8533954]},{'county':"Carroll","coordinates":[39.5423418,-77.0564464]},{'county':"Cecil","coordinates":[39.5739403,-75.94632399999999]},{'county':"Charles","coordinates":[38.5221781,-77.10249019999999]},{'county':"Dorchester","coordinates":[38.4152819,-76.17837390000001]},{'county':"Frederick","coordinates":[39.3844507,-77.4701972]},{'county':"Garrett","coordinates":[39.5681243,-79.29021329999999]},{'county':"Harford","coordinates":[39.5838964,-76.3637285]},{'county':"Howard","coordinates":[39.2873463,-76.964306]},{'county':"Kent","coordinates":[39.2713804,-76.1319953]},{'county':"Montgomery","coordinates":[39.1547426,-77.2405153]},{'county':"Prince George's","coordinates":[38.78492110000001,-76.8720961]},{'county':"Queen Anne's","coordinates":[39.0263572,-76.1319953]},{'county':"Somerset","coordinates":[38.0862333,-75.8533954]},{'county':"St. Mary's","coordinates":[38.1060259,-76.3637285]},{'county':"Talbot","coordinates":[38.7803973,-76.1319953]},{'county':"Washington","coordinates":[39.641762,-77.719993]},{'county':"Wicomico","coordinates":[38.3941813,-75.667356]},{'county':"Worcester","coordinates":[38.1584227,-75.4344727]},]
+    df =pd.read_sql_query('select * from vaccinations', con=engine)
+    # Converting Date column from String to Dates
+    df['DATE'] = df['DATE'].astype(str)
+    last_date = df[df['DATE'] == date]
+
+    # Populating Counties with number of cases for that specific date
+    for index,county in enumerate(counties):
+        cases = last_date[last_date['County'] == county['county']]['FullVaccinatedCumulative'].item()
+        counties[index]['date'] = date
+        counties[index]['FullVaccinatedCumulative'] = cases
+    # Appending cases and coordinates to Geojson data for Maryland Counties
+
+    # Build the endpoint URL
+    target_url = 'https://opendata.arcgis.com/datasets/4c172f80b626490ea2cff7b699febedb_1.geojson'
+    path_to_file = "./Resources/Maryland_Physical_Boundaries_-_County_Boundaries_(Generalized).geojson"
+    # generating request and co nverting to json
     geo_data = None
     with open(path_to_file) as f:
         geo_data = json.load(f)
@@ -41,8 +73,9 @@ def get_cases(date):
             geo_data['features'][county]['properties']['coordinates'] = counties[county]['coordinates']
             # setting confirmed cases and dates
             geo_data['features'][county]['properties']['date'] = counties[county]['date']
-            geo_data['features'][county]['properties']['confirmed_cases'] = counties[county]['Confirmed_cases']
+            geo_data['features'][county]['properties']['FullVaccinatedCumulative'] = counties[county]['FullVaccinatedCumulative']
     return json.dumps(geo_data)
+
 
 def get_vaccines_gender(date):
     df =pd.read_sql_query("SELECT * FROM gender", con=engine)
